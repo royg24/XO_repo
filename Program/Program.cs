b0b377bdbbf76e3dc88c0bde57e4c0840826dcf8
Roy Goldhar <roygl@mta.ac.il>
2023-05-12 09:02:29 +0300

    organize the code, change access modifiers

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,47 +1,17 @@
-﻿using Logics;
-using UserInterface;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static UserInterface.InterFace;
+using static UserInterface.GameActivation;
 
-namespace Program
+namespace ReversedTicTacToe
 {
     public class Program
     {
         public static void Main()
         {
-            UserInterface.InterFace uf = new UserInterface.InterFace();
-            do
-            {
-                uf.DurationOfGame();
-            } while (AskForAnotherGame() == true);
-        }
-        public static bool AskForAnotherGame()
-        {
-            string answer = null;
-            bool result;
-            Console.WriteLine(@"
-Do you want to play another game?
-1. Yes
-2. No");
-            answer = Console.ReadLine();
-            while (answer != "1" && answer != "2")
-            {
-                InvalidInputMessagePrint();
-                answer = Console.ReadLine();
-            }
-            if(answer == "1")
-            {
-                result = true;
-            }
-            else
-            {
-                result = false;
-            }
-            return result;
+            ActivateGame();
         }
     }
 }
